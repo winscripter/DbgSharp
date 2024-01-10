500d95c2f1b189f4bcbaebe0c49057034df682f2
winscripter <142818255+winscripter@users.noreply.github.com>
2024-01-10 20:29:38 +0400

    update

---
+++ DbgSharp/LocalizationHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using LangJsonSharp;
+
+namespace DbgSharp;
+
+internal static class LocalizationHelper
+{
+    private static MultipleLanguageManager Manager = new("DbgSharp");
+
+    public static void TryGetString(string name, out string value)
+    {
+        try
+        {
+            value = Manager[name]!;
+        }
+        catch
+        {
+            value = string.Empty;
+        }
+    }
+
+    public static void ChangeCulture(string cultureName)
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureName);
+        Manager = new("DbgSharp");
+    }
+
+    public static string GetString(string name)
+        => Manager[name]!;
+
+    public static string Resource(this string name)
+        => GetString(name);
+}
---
+++ DbgSharp/StandardOutputRedirect.cs
@@ -0,0 +1,36 @@
+﻿namespace DbgSharp;
+
+internal class StandardOutputRedirect : IDisposable
+{
+    private readonly StringWriter? _sw;
+    private bool _disposed = false;
+
+    public StandardOutputRedirect()
+    {
+        _sw = new();
+    }
+
+    public string Redirect(Action action)
+    {
+        TextWriter tw = Console.Out;
+        Console.SetOut(_sw!);
+
+        action();
+
+        Console.SetOut(tw);
+        tw.Dispose();
+
+        return _sw!.ToString();
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _disposed = true;
+            _sw!.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
